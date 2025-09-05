7dca444c9499c8d17f2fd73beed7554664259a86
EC2 Default User <ec2-user@ip-172-31-13-137.ec2.internal>
2025-09-05 15:50:58 +0000

    add wm install instructions

---
+++ install_watchmaker.sh
@@ -0,0 +1,36 @@
+# Could not get it to install w/o sudo
+#
+# scap profiles:
+#
+# (verify with: oscap info "/home/ec2-user/watchmaker/src/watchmaker/static/salt/formulas/scap-formula/scap/content/guides/openscap/ssg-al2023-ds.xml)"
+#
+# xccdf_org.ssgproject.content_profile_cis_server_l1
+# xccdf_org.ssgproject.content_profile_cis
+# xccdf_org.ssgproject.content_profile_stig
+#
+# You should be able to set the profile from config.yaml but was unable. Had to set it in:
+# watchmaker/src/watchmaker/static/salt/formulas/scap-formula/scap/oscap/scan.sls 
+
+sudo python3 -m ensurepip --upgrade
+
+sudo dnf install git
+python3 -m pip install build wheel setuptools
+
+git clone https://github.com/plus3it/watchmaker.git --recursive && cd watchmaker
+git submodule update --init --recursive
+git checkout 0.29.4
+sudo python3 -m pip install -e .
+
+sudo curl -fsSL https://github.com/saltstack/salt-install-guide/releases/latest/download/salt.repo \
+  | sudo tee /etc/yum.repos.d/salt.repo
+sudo dnf clean expire-cache
+sudo dnf install salt-minion salt-master -y
+sudo systemctl enable salt-minion
+sudo systemctl start salt-minion
+# sudo systemctl enable salt-master
+# sudo systemctl start salt-master
+
+sudo cp -R ~/watchmaker/src/watchmaker/static/salt/formulas/ash-linux-formula/ash-linux/el9 ~/watchmaker/src/watchmaker/static/salt/formulas/ash-linux-formula/ash-linux/el2023
+sudo cp -R ~/watchmaker/src/watchmaker/static/salt/formulas/ash-linux-formula/ash-linux/el2023 /srv/watchmaker/salt/formulas/ash-linux-formula/ash-linux
+
+sudo watchmaker –no-reboot --config /home/ec2-user/watchmaker/src/watchmaker/static/config.yaml
